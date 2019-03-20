2c788a9182643dae3d3ffbb4b62560e9ec8129e8
orihuzak <arx.solid@gmail.com>
2019-03-20 19:55:52 +0900

    万進法に対応

--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
-import { kansuji2number } from './src/kansuji2number.mjs'
+import { toNumber } from './src/kansuji2number.mjs'
 const log = console.log
 
-log(kansuji2number.toNumber('三百六十万二千十一'))
+log(toNumber('二億三千五百十一万四千二百九十二'))
+log(toNumber('五億三〇五万四千二〇〇'))
+log(toNumber('百二'))
--- src/kansuji2number.mjs
+++ src/kansuji2number.mjs
@@ -1,60 +1,63 @@
-const kansujiDict = { 〇: 0, 一: 1, 二: 2, 三: 3, 四: 4, 五: 5, 六: 6, 七: 7, 八: 8, 九: 9, 十: 10, 百: 100, 千: 1000, 万: 10000 },
+const kansujiDict = { 
+  〇: 0, 一: 1, 二: 2, 三: 3, 四: 4, 五: 5, 六: 6, 七: 7, 八: 8, 九: 9, 
+  十: 10, 百: 100, 千: 1000, 万: 10000, 億: 10 ** 8, 兆: 10 ** 12 },
       manRe = /[万億兆]/,
       unitRe =  /[十百千]/,
-      kansujiRe = /[〇一二三四五六七八九]/
+      log = console.log
 
-function toNumber(text){
-  return this.convert4(text, this.manRe, this.nonMan2number)
+export function toNumber(text){
+  return convert2(text, manRe)
 }
 
-function convert4(text, re, func) {
+function convert2(text, re) {
   let result = 0
+ // let flag = (manRe.test(text)) ? true : false
+  const flag = (manRe.test(text)) 
+    ? 1
+    : (unitRe.test(text))
+    ? 2
+    : 0
   while(re.test(text)) {
     const i = text.search(re)
     const prePtnStr = text.substring(0, i)
-    console.log(prePtnStr)
-    let prePtnNum = func(prePtnStr)
-    const kansuji = text[i]
-    text = text.substring(i + 1)
-    //console.log(text)
-    result += 1 * prePtnNum * kansujiDict[kansuji]
-  }
-  if (/[〇一二三四五六七八九]+/.test(text)) {
-    result += func(text)
-  }
-  return result
-}
-
-function convert3(text) {
-  let result = 0
-  // 万を探す
-  while(manRe.test(text)){
-    const i = text.search(manRe)
-    const preMan = text.substring(0, i)
-    let preManNumber = nonMan2number(preMan)
-    const man = text[i]
-    text = text.substring(i + 1)
-    result += 1 * preManNumber * kansujiDict[man]
-  }
-  if (/[〇一二三四五六七八九]+/.test(text)) {
-    result += nonMan2number(text)
-  }
-  return result
-}
-
-function nonMan2number(text) {
-  let result = 0
-  console.log(text)
-  while (unitRe.test(text)) {
-    const i = text.search(unitRe)
-    const preUnit = text.substring(0, i)
-    let preUnitNumber = nonUnitKansuji2number(preUnit, 1)
     const unit = text[i]
+
+    let prePtnNum = 0
+    if (flag) {
+      prePtnNum = convert(prePtnStr, unitRe)
+    } else prePtnNum = nonUnitKansuji2number(prePtnStr, 1)
+
     text = text.substring(i + 1)
-    result += 1 * preUnitNumber * kansujiDict[unit]
+    result += 1 * prePtnNum * kansujiDict[unit]
   }
   if (/[〇一二三四五六七八九]+/.test(text)) {
-    result += nonUnitKansuji2number(text)
+    result += (flag) 
+      ? convert(text, unitRe)
+      : nonUnitKansuji2number(text)
+  }
+  return result
+}
+
+function convert(text, re) {
+  let result = 0
+  let flag = (manRe.test(text)) ? true : false
+  while(re.test(text)) {
+    const i = text.search(re)
+    const prePtnStr = text.substring(0, i)
+    const unit = text[i]
+
+    let prePtnNum = 0
+    if (flag) {
+      prePtnNum = convert(prePtnStr, unitRe)
+    } else prePtnNum = nonUnitKansuji2number(prePtnStr, 1)
+
+    text = text.substring(i + 1)
+    result += 1 * prePtnNum * kansujiDict[unit]
+  }
+  if (/[〇一二三四五六七八九]+/.test(text)) {
+    result += (flag) 
+      ? convert(text, unitRe)
+      : nonUnitKansuji2number(text)
   }
   return result
 }
@@ -62,10 +65,10 @@ function nonMan2number(text) {
 /** 単位の漢数字を含まない漢数字列を数値化する。identityは単位元で0か１をとる */
 function nonUnitKansuji2number(text, identity = 0) {
   if (![0, 1].includes(identity)) throw new RangeError('identity must be 0 or 1.')
-    const kansujis = text.split('')
-    let result = 0
-    kansujis.forEach( (k, i) => {
-    result += (10 ** (kansujis.length - 1 - i)) * this.kansujiDict[k]
+  const kansujis = text.split('')
+  let result = 0
+  kansujis.forEach( (k, i) => {
+    result += (10 ** (kansujis.length - 1 - i)) * kansujiDict[k]
   })
   return (result === 0 && identity === 1) ? 1 : result
 }
