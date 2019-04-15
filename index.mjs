cc8572b769677ded30421a17e0486f536a19e044
arxsolid <arxsolid@penguin>
2019-04-15 18:31:36 +0900

    さらに短くした

--- index.mjs
+++ index.mjs
@@ -1,13 +1,9 @@
-import { kansuji2arabic } from "./src/kan2arabic";
+import { kansuji2arabic } from "./src/kansuji2arabic";
 const log = console.log
 
 log(kansuji2arabic('高度八千メートルの山と三百六十万二千十一の人々'))
-
-/*
-log(kansuji2Number('三百六十万二千十一'))
-log(kansuji2Number('二億三千五百十一万四千二百九十二'))
-log(kansuji2Number('五億三〇五万四千二〇〇'))
-log(kansuji2Number('百二'))
-log(kansuji2Number('十万千一'))
-log(kansuji2Number('十'))
-*/
+log(kansuji2arabic('二億三千五百十一万四千二百九十二'))
+log(kansuji2arabic('五億三〇五万四千二〇〇'))
+log(kansuji2arabic('百二'))
+log(kansuji2arabic('十万千一'))
+log(kansuji2arabic('十'))
--- src/kansuji2arabic.mjs
+++ src/kansuji2arabic.mjs
@@ -1,6 +1,5 @@
 export function kansuji2arabic(text) {
   const kansuji2number = { 零: 0, 〇: 0, 一: 1, 二: 2, 三: 3, 四: 4, 五: 5, 六: 6, 七: 7, 八: 8, 九: 9}
-  const reKansuji = /[零〇一二三四五六七八九]/
   const unit2number = {
     十: 10,
     百: 100,
@@ -9,26 +8,18 @@ export function kansuji2arabic(text) {
     億: 10 ** 8,
     兆: 10 ** 12
   }
-  // アラビア数字に変換
-  function kanUnit2arabic(text, unit) {
-    const reUnit = new RegExp(`(?<left>\\d*)${unit}(?<right>\\d*)`)
-    if(reUnit.test(text)) {
-      const match = text.match(reUnit)
-      const lNum = parseInt(match[1])
-      const rNum = parseInt(match[2])
-      const arabic = ((lNum ? lNum : 1) * unit2number[unit] + (rNum ? rNum : 0)).toString()
-      text = text.substring(0, match.index) + arabic + text.substring(match.index + match[0].length)
-      return kanUnit2arabic(text, unit)
-    } else return text
-  }
   // 文字列内の0-9までの漢数字をアラビア数字に変換
-  const chars = text.split('')
-  chars.forEach((char, i) => {
-    if(reKansuji.test(char)) chars[i] = kansuji2number[char]
-  })
-  let converted = chars.join('')
-  for (let unit of Object.keys(unit2number)) {
-    converted = kanUnit2arabic(converted, unit)
+  let arabic = text.replace(
+    /[零〇一二三四五六七八九]/g,
+    (kansuji) => kansuji2number[kansuji])
+  // 小さい単位から順にアラビア数字に変換
+  for (const unit of Object.keys(unit2number)) {
+    const reUnit = new RegExp(`(\\d*)${unit}(\\d*)`, 'g')
+    arabic = arabic.replace(reUnit, (...match) => {
+      const left = (match[1] ? parseInt(match[1]) : 1)
+      const right = (match[2] ? parseInt(match[2]) : 0)        
+      return (left * unit2number[unit] + right).toString()
+    })
   }
-  return converted
+  return arabic
 }
